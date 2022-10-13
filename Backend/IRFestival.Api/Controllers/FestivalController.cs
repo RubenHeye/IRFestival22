ba6cf2b8b974dfdde194bdfc351ce85c4b502bc4
Ruben Heye <Ruben.Heye@inetum-realdolmen.world>
2022-10-13 10:47:02 +0200

    [UPD] Lineup from db

--- Backend/IRFestival.Api/Controllers/FestivalController.cs
+++ Backend/IRFestival.Api/Controllers/FestivalController.cs
@@ -26,7 +26,7 @@ namespace IRFestival.Api.Controllers
             var festival = await _ctx.Festivals.FirstOrDefaultAsync();
             var schedule = await _ctx.Schedules.Where(x => x.FestivalId.Equals(festival.Id)).FirstOrDefaultAsync();
             var scheduleItems = await _ctx.ScheduleItems.Where(x => x.ScheduleId.Equals(schedule.Id)).ToListAsync();
-            return Ok(FestivalDataSource.Current.LineUp);
+            return Ok(scheduleItems);
         }
 
         [HttpGet("Artists")]
