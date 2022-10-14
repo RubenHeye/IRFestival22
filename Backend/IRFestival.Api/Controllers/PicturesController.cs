4c7e892c3b90ad1d78a44e761066e5d688113b5d
Ruben Heye <Ruben.Heye@inetum-realdolmen.world>
2022-10-14 09:29:52 +0200

    [UPD] Get and upload pictures

--- Backend/IRFestival.Api/Controllers/PicturesController.cs
+++ Backend/IRFestival.Api/Controllers/PicturesController.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using IRFestival.Api.Common;
+using IRFestival.Api.Options;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Net;
+using System.Web;
 
 namespace IRFestival.Api.Controllers
 {
@@ -8,6 +13,25 @@ namespace IRFestival.Api.Controllers
     [ApiController]
     public class PicturesController : ControllerBase
     {
+        public BlobUtility BlobUtility { get; }
+
+        public PicturesController(BlobUtility blobUtility)
+        {
+            BlobUtility = blobUtility;
+        }
+
+        [HttpGet("getUrls")]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string[]))]
+        public async Task<ActionResult> GetAllPicturesUrls()
+        {
+            var container = BlobUtility.GetPicturesContainer();
+            var result = container.GetBlobs()
+                .Select(blob => BlobUtility.GetSasUri(container, blob.Name))
+                .ToArray();
+
+            return Ok(result);
+        }
+
         [HttpGet]
         public string[] GetAllPictureUrls()
         {
@@ -15,8 +39,14 @@ namespace IRFestival.Api.Controllers
         }
 
         [HttpPost]
-        public void PostPicture(IFormFile file)
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(AppSettingsOptions))]
+        public async Task<ActionResult> PostPicture(IFormFile file)
         {
+            BlobContainerClient container = BlobUtility.GetPicturesContainer();
+            var filename = $"{DateTimeOffset.UtcNow.ToUnixTimeSeconds()}{HttpUtility.UrlPathEncode(file.FileName)}";
+            await container.UploadBlobAsync(filename, file.OpenReadStream());
+
+            return Ok();
         }
     }
 }
---
+++ Backend/IRFestival.Api/Options/BlobSettingsOptions.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+
+namespace IRFestival.Api.Options
+{
+    public class BlobSettingsOptions
+    {
+        public string ThumbsContainer { get; set; }
+        public string PicturesContainer { get; set; }
+    }
+}
